4eac0a782af6f589cd4c00e6bb56663f702dc10b
Szabolcs Rajcsányi <rajszabi@gmail.com>
2025-06-19 16:52:40 +0200

    enhance chatbot function signature to include previous messages and update return type

--- backend/src/agents/chatbot.py
+++ backend/src/agents/chatbot.py
@@ -2,9 +2,17 @@ from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import MessagesState
 from langchain_core.runnables import RunnableConfig
 from langgraph.func import entrypoint
-
+from langchain_core.messages import BaseMessage
 
 
 @entrypoint(checkpointer=MemorySaver())
-async def chatbot(input: MessagesState, config: RunnableConfig):
+async def chatbot(
+    input: MessagesState, 
+    previous: list[BaseMessage], 
+    config: RunnableConfig) -> entrypoint.final[
+        dict[str, list[BaseMessage]],
+        dict[str, list[BaseMessage]]
+    ]:
+    messages = input.messages
+
     
